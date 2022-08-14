f25ea9ddfda611b1234d0b89376197651b7151d0
solomonkifle <102066454+solomonkifle@users.noreply.github.com>
2022-08-14 17:30:35 +0300

    Update 105-radix_sort.c

--- 105-radix_sort.c
+++ 105-radix_sort.c
@@ -1,56 +1,78 @@
 #include "sort.h"
 
 /**
- * radix_sort - sorts an array following the Radix sort algorithm
- * @array: array of ints to sort
- * @size: size of the array
+ * get_digit - gets a digit from a number
+ * @number: the integer
+ * @digit: the digit position to get
+ *
+ * Return: the digit value at given position
+**/
+int get_digit(long number, int digit)
+{
+	long i = 0L, pow = 1L, ret;
+
+	for (i = 0; i < digit; i++)
+		pow *= 10L;
+	ret = ((number / pow) % 10);
+	return (ret);
+}
+
+/**
+ * radix_pass - sorts by radix
+ * @array: the integer array to sort
+ * @size: the size of the array
+ * @digit: the current digit to check
+ * @new_array: target array of same size
+ *
+ * Return: void.
+ */
+int radix_pass(int *array, ssize_t size, int digit, int *new_array)
+{
+	ssize_t i;
+	int buckets[10] = {0};
+
+	for (i = 0; i < size; i++)
+		buckets[get_digit(array[i], digit)]++;
+	for (i = 1; i <= 9; i++)
+		buckets[i] += buckets[i - 1];
+	for (i = size - 1; i > -1; i--)
+		new_array[buckets[get_digit(array[i], digit)]-- - 1] = array[i];
+	return (1);
+}
+
+/**
+ * radix_sort - sorts by radix
+ * @size: the size of the array
+ * @array: the integer array to sort
+ *
+ * Return: the gap size
  */
 void radix_sort(int *array, size_t size)
 {
-	int max;
-	size_t i, lsd;
+	int *old_array, *new_array, *temp_ptr, *ptr, max = 0;
+	size_t i, sd = 1;
 
 	if (!array || size < 2)
 		return;
 
-	max = 0;
 	for (i = 0; i < size; i++)
 		if (array[i] > max)
 			max = array[i];
-
-	for (lsd = 1; max / lsd > 0; lsd *= 10)
+	while (max /= 10)
+		sd++;
+	old_array = array;
+	new_array = ptr = malloc(sizeof(int) * size);
+	if (!new_array)
+		return;
+	for (i = 0; i < sd; i++)
 	{
-		count_sort_LSD(array, size, lsd);
-		print_array(array, size);
+		radix_pass(old_array, (ssize_t)size, i, new_array);
+		temp_ptr = old_array;
+		old_array = new_array;
+		new_array = temp_ptr;
+		print_array(old_array, size);
 	}
-}
-
-/**
- * count_sort_LSD - count sort with LSD
- * @array: array to sort
- * @size: size of the array
- * @lsd: least significant digit
- */
-void count_sort_LSD(int *array, size_t size, size_t lsd)
-{
-	int count_arr[10] = {0}, *out_arr, l, m;
-	size_t k, n;
-
-	out_arr = malloc(sizeof(int) * size);
-
-	for (k = 0; k < size; k++)
-		count_arr[(array[k] / lsd) % 10]++;
-	for (l = 1; l < 10; l++)
-		count_arr[l] += count_arr[l - 1];
-
-	for (m = size - 1; m >= 0; m--)
-	{
-		out_arr[count_arr[(array[m] / lsd) % 10] - 1] = array[m];
-		count_arr[(array[m] / lsd) % 10]--;
-	}
-
-	for (n = 0; n < size; n++)
-		array[n] = out_arr[n];
-
-	free(out_arr);
+	for (i = 0; i < size; i++)
+		array[i] = old_array[i];
+	free(ptr);
 }
